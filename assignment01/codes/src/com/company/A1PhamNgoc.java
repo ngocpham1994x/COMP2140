668d3296a96f471b67a2be1d840c80fac1d59993
Ngoc Pham <ngoc.pham.nm@gmail.com>
2021-02-15 21:57:47 -0600

    add assignment01

---
+++ assignment01/codes/src/com/company/A1PhamNgoc.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class A1PhamNgoc {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+
+}
+
+
+/*Report:
+1.
+2.
+3.
+4.
+5.
+6.
+ */
--- lab02-quickSort/codes/src/com/company/QuickSort.java
+++ lab02-quickSort/codes/src/com/company/QuickSort.java
@@ -127,4 +127,41 @@ public class QuickSort {
 
     }
 
+
+    public static void main(String[] args) {
+        // write your code here
+        QuickSort arr = new QuickSort();
+
+        System.out.println("Testing array setups. Adding items to array: ");
+        arr.addToArray();
+        arr.addToArray();
+        arr.addToArray();
+        arr.addToArray();
+        arr.addToArray();
+
+
+        System.out.println("Capacity: " + arr.getCapacity());
+        System.out.println("Current size: " + arr.getSize());
+        System.out.println(arr.toString());
+        arr.clear();
+        System.out.println("Clear array");
+        System.out.println(arr.toString());
+
+
+        System.out.println("\n \nNew array");
+        for(int i = 0; i < arr.getCapacity(); i++){
+            arr.addToArray();
+        }
+        System.out.println("Capacity: " + arr.getCapacity());
+        System.out.println("Current size: " + arr.getSize());
+        System.out.println(arr.toString());
+
+
+        System.out.println("Sorted array");
+        arr.quickSort();
+        System.out.println(arr.toString());
+
+
+    }
+
 }//end QuickSort class
